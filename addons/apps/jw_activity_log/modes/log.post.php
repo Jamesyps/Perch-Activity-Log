c5a99c15ac981a099453dc7f546fdfd7de25808b
James Wigger <james@rootstudio.co.uk>
2015-09-02 16:17:38 +0100

    Allow users to filter by other methods when no results returned

--- addons/apps/jw_activity_log/modes/log.post.php
+++ addons/apps/jw_activity_log/modes/log.post.php
@@ -12,11 +12,8 @@ echo $HTML->main_panel_start();
 include('_subnav.php');
 
 echo $HTML->heading1('Recent Actions');
-if (isset($message)) echo $message;
-
 ?>
 
-<?php if(PerchUtil::count($action_logs)): ?>
 <div class="jw-activity-log">
     <ul class="smartbar">
         <li class="<?php if($filter == '*'): ?>selected<?php endif; ?>"><a href="<?php echo PerchUtil::html($API->app_path()); ?>"><?php echo $Lang->get('All'); ?></a></li>
@@ -41,73 +38,75 @@ if (isset($message)) echo $message;
         <?php endif; ?>
     </ul>
 
-    <table class="d log-table">
-        <thead>
-            <tr>
-                <th class="icon">&nbsp;</th>
-                <th>
-                    <?php echo $Lang->get('Action'); ?>
-                </th>
-                <th>
-                    <?php echo $Lang->get('Type'); ?>
-                </th>
-                <th>
-                    <?php echo $Lang->get('Title'); ?>
-                </th>
-                <th>
-                    <?php echo $Lang->get('Account'); ?>
-                </th>
-                <th>
-                    <?php echo $Lang->get('Role'); ?>
-                </th>
-                <th>
-                    <?php echo $Lang->get('Date / Time'); ?>
-                </th>
-                <th class="action last">&nbsp;</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($action_logs as $Action): ?>
+    <?php if(isset($message)) echo $message; ?>
+
+    <?php if(PerchUtil::count($action_logs)): ?>
+        <table class="d log-table">
+            <thead>
                 <tr>
-                    <td class="icon">
-                        <img src="<?php echo $Action->icon(); ?>" alt="<?php echo $HTML->encode($Action->actionKey()); ?>" />
-                    </td>
-                    <td>
-                        <?php echo $HTML->encode($Action->actionKeyFormat()); ?>
-                    </td>
-                    <td>
-                        <?php echo $HTML->encode(ucfirst($Action->resourceType())); ?>
-                    </td>
-                    <td>
-                        <?php echo $HTML->encode($Action->resourceTitle()); ?>
-                    </td>
-                    <td>
-                        <?php echo $HTML->encode($Action->userProperty('userUsername', 'N/A')); ?>
-                    </td>
-                    <td>
-                        <?php echo $HTML->encode($Action->userProperty('roleTitle', 'N/A')); ?>
-                    </td>
-                    <td>
-                        <span title="<?php echo $HTML->encode($Action->actionDateTime()); ?>">
-                            <?php echo $HTML->encode($Action->relativeTime()); ?>
-                        </span>
-                    </td>
-                    <td>
-                        <a class="action" href="<?php echo $HTML->encode($API->app_path()); ?>/view/?id=<?php echo $HTML->encode(urlencode($Action->id())); ?>">
-                            <?php echo $Lang->get('Details'); ?>
-                        </a>
-                    </td>
+                    <th class="icon">&nbsp;</th>
+                    <th>
+                        <?php echo $Lang->get('Action'); ?>
+                    </th>
+                    <th>
+                        <?php echo $Lang->get('Type'); ?>
+                    </th>
+                    <th>
+                        <?php echo $Lang->get('Title'); ?>
+                    </th>
+                    <th>
+                        <?php echo $Lang->get('Account'); ?>
+                    </th>
+                    <th>
+                        <?php echo $Lang->get('Role'); ?>
+                    </th>
+                    <th>
+                        <?php echo $Lang->get('Date / Time'); ?>
+                    </th>
+                    <th class="action last">&nbsp;</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($action_logs as $Action): ?>
+                    <tr>
+                        <td class="icon">
+                            <img src="<?php echo $Action->icon(); ?>" alt="<?php echo $HTML->encode($Action->actionKey()); ?>" />
+                        </td>
+                        <td>
+                            <?php echo $HTML->encode($Action->actionKeyFormat()); ?>
+                        </td>
+                        <td>
+                            <?php echo $HTML->encode(ucfirst($Action->resourceType())); ?>
+                        </td>
+                        <td>
+                            <?php echo $HTML->encode($Action->resourceTitle()); ?>
+                        </td>
+                        <td>
+                            <?php echo $HTML->encode($Action->userProperty('userUsername', 'N/A')); ?>
+                        </td>
+                        <td>
+                            <?php echo $HTML->encode($Action->userProperty('roleTitle', 'N/A')); ?>
+                        </td>
+                        <td>
+                            <span title="<?php echo $HTML->encode($Action->actionDateTime()); ?>">
+                                <?php echo $HTML->encode($Action->relativeTime()); ?>
+                            </span>
+                        </td>
+                        <td>
+                            <a class="action" href="<?php echo $HTML->encode($API->app_path()); ?>/view/?id=<?php echo $HTML->encode(urlencode($Action->id())); ?>">
+                                <?php echo $Lang->get('Details'); ?>
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php
+        if ($Paging->enabled()) {
+            echo $HTML->paging($Paging);
+        }
+        ?>
+    <?php endif; ?>
 </div>
 
-<?php
-if ($Paging->enabled()) {
-    echo $HTML->paging($Paging);
-}
-?>
-
-<?php endif; ?>
 <?php echo $HTML->main_panel_end(); ?>
